83120a6149bbacbb3e75239979a4e90091ad8618
Fuad J Torres <Fuadjtorres99@gmail.com>
2023-01-08 02:04:48 -0700

    Finished .. i think

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage: ");
+        string percent = Console.ReadLine();
+        int num = int.Parse(percent);
+        Boolean letter = true;
+
+        // num holds the percent now
+
+        if (num >= 90) {Console.WriteLine("You got an A."); letter = true;}
+        else if (num >= 80) {Console.WriteLine("You got an B."); letter = true;}
+        else if (num >= 70) {Console.WriteLine("You got an C."); letter = true;}
+        else if (num >= 60) {Console.WriteLine("You got an D."); letter = false;}
+        else if (num <  60) {Console.WriteLine("You got an F."); letter = false;}
+
+        if (letter) {Console.Write("HURAY!!! you passed.");}
+        else {Console.Write("Sorry, you faild.");}
+       
+
     }
 }
