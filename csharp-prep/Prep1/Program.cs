2dda1a9b32014930909ca01f372d32a3f1e6317a
Fuad J Torres <Fuadjtorres99@gmail.com>
2023-01-08 01:07:16 -0700

    FINISHED IT

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    
+        Console.Write("What is your first name?");
+        string firstN = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lastN = Console.ReadLine();
+        Console.WriteLine($"your name is, {firstN} {lastN}.");
     }
 }
