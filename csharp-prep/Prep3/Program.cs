d104435170001af31152a8aa968a4383dd91fc8a
Fuad J Torres <Fuadjtorres99@gmail.com>
2023-01-11 15:51:43 -0700

    finished the assinment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,21 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int Mnum = int.Parse(magicNumber);
+        int Gnum = 0;
+
+        while (Gnum != Mnum) 
+        {   
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            Gnum = int.Parse(guessNumber);
+
+            if (Gnum > Mnum) {Console.WriteLine("Lower");}
+            else if (Gnum < Mnum) {Console.WriteLine("Higher");}
+            else {Console.WriteLine("You guessed it!");}
+        }
     }
 }
