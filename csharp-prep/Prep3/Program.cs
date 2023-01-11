9af3e5eb31254bb9299f5666d967cb8950ed408e
Fuad J Torres <Fuadjtorres99@gmail.com>
2023-01-11 16:40:01 -0700

    1 streach chalinge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,20 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {   
-        Console.Write("What is the magic number? ");
-        string magicNumber = Console.ReadLine();
-        int Mnum = int.Parse(magicNumber);
+        Console.WriteLine("Time to play Guess MY Number");
+        Random randomGenerator = new Random();
+        int Mnum = randomGenerator.Next(1, 100);
         int Gnum = 0;
-
+        int guesses = 0;
+        string play = "yes";
+    
         while (Gnum != Mnum) 
         {   
             Console.Write("What is your guess? ");
             string guessNumber = Console.ReadLine();
             Gnum = int.Parse(guessNumber);
+            guesses = guesses + 1;
 
             if (Gnum > Mnum) {Console.WriteLine("Lower");}
             else if (Gnum < Mnum) {Console.WriteLine("Higher");}
-            else {Console.WriteLine("You guessed it!");}
+            else 
+            { 
+                Console.WriteLine("You guessed it!");
+                guesses.ToString();
+                Console.WriteLine($"it took you {guesses} tryes.");
+            }
         }
+
+        
     }
 }
