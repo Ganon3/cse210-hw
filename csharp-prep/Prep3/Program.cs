ff859de2935bbd6028c92e2e1706f777a9a6a79b
Fuad J Torres <Fuadjtorres99@gmail.com>
2023-01-11 17:08:00 -0700

    did all chalinges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,30 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {   
+        string play = "no";
         Console.WriteLine("Time to play Guess MY Number");
-        Random randomGenerator = new Random();
-        int Mnum = randomGenerator.Next(1, 100);
-        int Gnum = 0;
-        int guesses = 0;
-        string play = "yes";
-    
-        while (Gnum != Mnum) 
+        do 
         {   
-            Console.Write("What is your guess? ");
-            string guessNumber = Console.ReadLine();
-            Gnum = int.Parse(guessNumber);
-            guesses = guesses + 1;
+             
+            Random randomGenerator = new Random();
+            int Mnum = randomGenerator.Next(1, 100);
+            int Gnum = 0;
+            int guesses = 0;
 
-            if (Gnum > Mnum) {Console.WriteLine("Lower");}
-            else if (Gnum < Mnum) {Console.WriteLine("Higher");}
-            else 
-            { 
-                Console.WriteLine("You guessed it!");
-                guesses.ToString();
-                Console.WriteLine($"it took you {guesses} tryes.");
+            while (Gnum != Mnum) 
+            {   
+               Console.Write("What is your guess? ");
+               string guessNumber = Console.ReadLine();
+               Gnum = int.Parse(guessNumber);
+               guesses = guesses + 1;
+
+               if (Gnum > Mnum) {Console.WriteLine("Lower");}
+               else if (Gnum < Mnum) {Console.WriteLine("Higher");}
+               else 
+               { 
+                   Console.WriteLine("You guessed it!");
+                   guesses.ToString();
+                   Console.WriteLine($"it took you {guesses} tryes.");
+               }
             }
-        }
+            Console.Write("Do you want to continue? ");
+            play = Console.ReadLine();
 
-        
+        } while (play == "yes");
     }
 }
