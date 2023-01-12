71a8dc65fb55a65cce5feaf4b9988faf36372eb0
Fuad J Torres <Fuadjtorres99@gmail.com>
2023-01-12 16:24:12 -0700

    made a commen6

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         
-        
+        // comment
         static string PromptUserName()
         {
             Console.Write("Please enter your name: ");
