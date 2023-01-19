20ad59325f14315b4630d717d0fc52c03d7f24b1
Fuad J Torres <Fuadjtorres99@gmail.com>
2023-01-18 19:41:12 -0700

    small fix

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,12 +8,12 @@ class Program
         job1._jobTitle = "Software Engineer";
         job1._company = "Apple";
         // Console.WriteLine(job1._jobTitle);
-        job1.Display();
+        // job1.Display();
 
         Job job2 = new Job();
         job2._jobTitle = "Software Engineer";
         job2._company = "Microsoft";
-        job2.Display();
+        // job2.Display();
 
         Resume rresume = new Resume();
 
